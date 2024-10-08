22b1c6be96fc2823b664b992ba9f625e05a3fc93
Mauricio-Mtz <maurimtz07@gmail.com>
2024-10-07 22:24:56 -0600

    Proceso de instalacion de dependencias

---
+++ install-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Habilita el modo para detener el script si ocurre algún error
+set -e
+
+# Instalando dependencias en gateway
+echo "Instalando dependencias en gateway..."
+cd gateway
+npm install
+cd ..
+
+# Instalando dependencias en auth
+echo "Instalando dependencias en auth..."
+cd auth
+npm install
+cd ..
+
+echo "Instalación de dependencias completada!"
