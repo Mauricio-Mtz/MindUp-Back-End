3ecc7850fdf97650c7e270f20911f3a31f721832
Mauricio-Mtz <maurimtz07@gmail.com>
2024-10-20 20:49:53 -0600

    Correccion a las variables de entorno

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,15 +15,15 @@ module.exports = {
           NODE_ENV: 'production',
           AUTH_PORT: process.env.AUTH_PORT || 3001,
           
-          DB_HOST: process.env.DB_HOST || 'localhost',
-          DB_USER: process.env.DB_USER || 'root',
-          DB_PASSWORD: process.env.DB_PASSWORD || '1224',
-          DB_NAME: process.env.DB_NAME || 'mindup',
+          // DB_HOST: process.env.DB_HOST || 'localhost',
+          // DB_USER: process.env.DB_USER || 'root',
+          // DB_PASSWORD: process.env.DB_PASSWORD || '1224',
+          // DB_NAME: process.env.DB_NAME || 'mindup',
 
-          // DB_HOST: process.env.DB_HOST || 'codeflex.space',
-          // DB_USER: process.env.DB_USER || 'codeflex_mauricio',
-          // DB_PASSWORD: process.env.DB_PASSWORD || 'Mauricio1224!',
-          // DB_NAME: process.env.DB_NAME || 'codeflex_mindup',
+          DB_HOST: process.env.DB_HOST || 'codeflex.space',
+          DB_USER: process.env.DB_USER || 'codeflex_mauricio',
+          DB_PASSWORD: process.env.DB_PASSWORD || 'Mauricio1224!',
+          DB_NAME: process.env.DB_NAME || 'codeflex_mindup',
           
           JWT_SECRET: process.env.JWT_SECRET || 'mysecretkey'
         }
@@ -35,15 +35,15 @@ module.exports = {
           NODE_ENV: 'production',
           COURSES_PORT: process.env.AUTH_PORT || 3002,
           
-          DB_HOST: process.env.DB_HOST || 'localhost',
-          DB_USER: process.env.DB_USER || 'root',
-          DB_PASSWORD: process.env.DB_PASSWORD || '1224',
-          DB_NAME: process.env.DB_NAME || 'mindup',
+          // DB_HOST: process.env.DB_HOST || 'localhost',
+          // DB_USER: process.env.DB_USER || 'root',
+          // DB_PASSWORD: process.env.DB_PASSWORD || '1224',
+          // DB_NAME: process.env.DB_NAME || 'mindup',
 
-          // DB_HOST: process.env.DB_HOST || 'codeflex.space',
-          // DB_USER: process.env.DB_USER || 'codeflex_mauricio',
-          // DB_PASSWORD: process.env.DB_PASSWORD || 'Mauricio1224!',
-          // DB_NAME: process.env.DB_NAME || 'codeflex_mindup',
+          DB_HOST: process.env.DB_HOST || 'codeflex.space',
+          DB_USER: process.env.DB_USER || 'codeflex_mauricio',
+          DB_PASSWORD: process.env.DB_PASSWORD || 'Mauricio1224!',
+          DB_NAME: process.env.DB_NAME || 'codeflex_mindup',
           
           JWT_SECRET: process.env.JWT_SECRET || 'mysecretkey'
         }
